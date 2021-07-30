475faef899083804e781ac205aa8bb61e62f976c
Ivan Orfanidi <i.orfanidi@mail.ru>
2021-07-30 13:13:55 +0300

    Добавлен параметр дискретизации обработки номера фрейма.

--- source/main.cpp
+++ source/main.cpp
@@ -14,7 +14,7 @@ constexpr std::string_view NAME_MODEL_FILE = "MobileNetSSD_deploy.caffemodel"; /
 
 constexpr int WIDTH = 500;
 constexpr int HEIGHT = 500;
-constexpr int DELAY_MS = 10;
+constexpr int DELAY_MS = 1;
 
 void getLabelsFromFile(std::vector<std::string>& labels, const std::string& nameFile)
 {
@@ -39,12 +39,14 @@ int main(int argc, char* argv[])
     std::string inputFile;
     std::string outputFile;
     bool useCuda;
+    uint16_t frameNumber;
     boost::program_options::options_description desc("Options");
     desc.add_options()
         // All options:
         ("in,i", boost::program_options::value<std::string>(&inputFile)->default_value(""), "Path to input file.\n") //
         ("out,o", boost::program_options::value<std::string>(&outputFile)->default_value("output.mp4"), "Path to output file.\n") //
         ("cuda,c", boost::program_options::value<bool>(&useCuda)->default_value(true), "Set CUDA Enable.\n") //
+        ("frame,f", boost::program_options::value<uint16_t>(&frameNumber)->default_value(1), "Set frame number.") //
         ("help,h", "Produce help message."); // Help
     boost::program_options::variables_map options;
     try {
@@ -102,10 +104,14 @@ int main(int argc, char* argv[])
     while (cv::waitKey(DELAY_MS) != ESCAPE_KEY) {
         // Read a new frame from video.
         cv::Mat source;
-        if (capture.read(source) == false) {
-            std::cerr << "Video camera is disconnected!" << std::endl;
-            return EXIT_FAILURE;
-        }
+        uint16_t i = 0;
+        do {
+            if (capture.read(source) == false) {
+                std::cerr << "Video camera is disconnected!" << std::endl;
+                return EXIT_FAILURE;
+            }
+            ++i;
+        } while (i < frameNumber);
         resize(source, source, cv::Size(WIDTH, HEIGHT), 0, 0);
 
         cv::dnn::Net neuralNetwork;
